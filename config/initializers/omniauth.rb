7950754ced7a9169769d48039a0a2a329774ebc1
Michael Voetter <Michael.Voetter@student.uibk.ac.at>
2014-10-24 22:27:01 +0200

    add google oauth2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['SCHEUDLER_FACEBOOK_ID'], ENV['SCHEUDLER_FACEBOOK_AUTH_PASSWORD'], secure_image_url: true
+  provider :google_oauth2, ENV["SCHEUDLER_GOOGLE_ID"] , ENV["SCHEUDLER_GOOGLE_AUTH_PASSWORD"]
 end
