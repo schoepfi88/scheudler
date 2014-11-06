d76a9d7fd0529b43cae2db967e74ca8c848692c5
primersion <blingwing093@gmail.com>
2014-11-06 13:47:30 +0100

    Added simple groups to db and fixed create group page

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021161209) do
+ActiveRecord::Schema.define(version: 20141106124302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: true do |t|
+    t.string "name"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
