b53d0df7a04da064f3b94bf3ed2433b7e6e8f06f
primersion <blingwing093@gmail.com>
2014-11-07 15:42:47 +0100

    Errors fixed

--- app/assets/javascripts/controller/groups.js
+++ app/assets/javascripts/controller/groups.js
@@ -1,6 +1,6 @@
 angular.module('scheudler').controller("groupsCtrl", function($scope,groupsService,Util,$location,$route){
 	$scope.submit = function(){
-		if($scope.groupData.name != '' && $scope.groupData.description != ''){
+		if($scope.groupData.name !== '' && $scope.groupData.description !== ''){
 			groupsService.group.create($scope.groupData, function(){
 				Util.redirect_to.back('/groups');
 				window.location.reload();
