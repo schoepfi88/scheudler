594eaba8953b77d70eb6ab42638a75de2bb7bc81
Roman Reinold <roman.reinold@student.uibk.ac.at>
2014-11-16 09:04:37 +0000

    added events to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113113321) do
+ActiveRecord::Schema.define(version: 20141114182404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,22 @@ ActiveRecord::Schema.define(version: 20141113113321) do
     t.datetime "updated_at"
   end
 
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "length"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "groups", force: true do |t|
     t.string   "name"
     t.string   "description"
-    t.string   "icon"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "admin_id"
   end
 
   create_table "members", force: true do |t|
