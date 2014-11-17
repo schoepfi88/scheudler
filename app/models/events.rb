978ee7ba707242d8c474ddccbef805ae78db6479
Roman Reinold <roman.reinold@student.uibk.ac.at>
2014-11-17 10:24:11 +0000

    add participants

--- app/models/events.rb
+++ app/models/events.rb
@@ -1,2 +1,4 @@
 class Events < ActiveRecord::Base
+	has_many :participants
+	has_many :users, :through => :participants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114182404) do
+ActiveRecord::Schema.define(version: 20141117101918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20141114182404) do
 
   create_table "members", force: true do |t|
     t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "participants", force: true do |t|
+    t.integer  "event_id"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
