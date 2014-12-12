63619a6d3ca4f37a35cbeee1775b1e2bf25060e3
schoepfi88 <ch.schoepfi1@hotmail.com>
2014-12-12 17:51:53 +0100

    dashboad view my groups update

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,8 @@ ActiveRecord::Schema.define(version: 20141202123708) do
   enable_extension "plpgsql"
 
   create_table "admins", force: true do |t|
-    t.integer  "group_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "group_id"
+    t.integer "user_id"
   end
 
   create_table "events", force: true do |t|
@@ -34,19 +32,24 @@ ActiveRecord::Schema.define(version: 20141202123708) do
     t.integer  "group_id"
   end
 
-  create_table "groups", force: true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.string   "icon"
+  create_table "events_groups", force: true do |t|
+    t.integer  "event_id"
+    t.integer  "groups_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "members", force: true do |t|
-    t.integer  "group_id"
-    t.integer  "user_id"
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "icon"
+  end
+
+  create_table "members", force: true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
   end
 
   create_table "participants", force: true do |t|
