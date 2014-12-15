a83b310685eeb23eea9aedb5b21f5cab41914642
schoepfi88 <ch.schoepfi1@hotmail.com>
2014-12-15 17:27:46 +0100

    message on dashboard sensational design ;)

--- app/assets/javascripts/controller/dashboard.js
+++ app/assets/javascripts/controller/dashboard.js
@@ -1,6 +1,12 @@
 angular.module('scheudler').controller("dashboardCtrl",
     function($scope,Util,dashboardService){
 
-    $scope.messages = dashboardService.user.get();
-
+	$scope.messages = dashboardService.user.get();
+	// set correct line-height to center the pic and the time
+	var element = document.getElementById('mes-text');
+	var pic = document.getElementById('mes-pic');
+	var time = document.getElementById('mes-time');
+	pic.style.lineHeight = element.offsetHeight.toString() + "px";
+	time.style.lineHeight = element.offsetHeight.toString() + "px";
+	
 });
