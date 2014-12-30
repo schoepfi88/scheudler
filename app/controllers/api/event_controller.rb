23d5744cadc8b7332316eccc50194f52573c68d0
Michael Voetter <Michael.Voetter@student.uibk.ac.at>
2014-12-30 18:28:32 +0100

    add calendar api functionality

--- app/controllers/api/event_controller.rb
+++ app/controllers/api/event_controller.rb
@@ -1,15 +1,115 @@
 class Api::EventController < Api::RestController
+	require 'google/api_client'
+	
+	API_VERSION = 'v3'
+  CALENDAR_ID = 'bqh0i24famv71aaa84mneavovg@group.calendar.google.com'
    
-  def index
-    client = Google::APIClient.new
-    client.authorization.access_token = session[:token]
-    service = client.discovered_api('calendar', 'v3')
-    result = client.execute(
-      :api_method => service.events.list,
-      :parameters => {'calendarId' => 'bqh0i24famv71aaa84mneavovg@group.calendar.google.com'},
-      :headers => {'Content-Type' => 'application/json'})
-     
-    @events = result.data.items
-    respond_with @events
+  def index  
+		init_calendar
+   
+    events = gcal_events_get(CALENDAR_ID)
+		
+		out = Array.new
+
+		events.each do |event|
+			ev = Event.new
+			ev.gcal_id = event.id
+			ev.name = event.summary
+			ev.start = event.start
+			ev.end = event.end
+
+			out.push(ev.to_json)
+		end
+
+    respond_with gcal_acl_list("7859cgudueuebe4p7pum00gpao@group.calendar.google.com")
   end
+
+private
+	def gcal_events_get(gcal_id)
+		params = {
+      calendarId: gcal_id
+    }
+    result = @client.execute(
+      :api_method => @calendar.events.list,
+      :parameters => params,
+      :headers => {'Content-Type' => 'application/json'}
+		)
+		return result.data.items
+	end
+
+	def gcal_event_insert(gcal_id, event)
+		params = {
+      calendarId: gcal_id
+    }
+    result = @client.execute(
+      :api_method => @calendar.events.insert,
+      :parameters => params,
+      :body_object => event.convert_to_gcal_event
+    )
+	end
+
+	def gcal_event_update(gcal_id, event)
+    params = {
+      calendarId: gcal_id,
+      eventId: event.gcal_id
+    }
+    result = @client.execute(
+      :api_method => calendar.events.update,
+      :parameters => params,
+      :body_object => event.convert_to_gcal_event
+    )
+  end
+
+	def gcal_event_delete(gcal_id, event_id)
+    params = {
+      calendarId: gcal_id,
+      eventId: event_id
+    }
+    result = @client.execute(
+      :api_method => @calendar.events.delete,
+      :parameters => params
+    )
+  end
+
+	def gcal_calendar_list
+		result = @client.execute(
+      :api_method => @calendar.calendar_list.list
+    )
+		return result.data.items
+	end
+
+	def gcal_calendar_create(group_name)
+		cal = {
+			"kind" => "calendar#calendar",
+			"summary" => "Scheudler " + group_name + " Calendar",
+			"description" =>"Scheudler group calendar",
+			"timeZone" => "Europe/Vienna"
+		}
+		result = @client.execute(
+		  :api_method => @calendar.calendars.insert,
+		  :body_object => cal
+		)
+		return result.data.id
+	end
+
+	def gcal_acl_list(gcal_id)
+		params = {
+      calendarId: gcal_id
+    }
+    result = @client.execute(
+      :api_method => @calendar.acl.list,
+      :parameters => params
+    )
+		return result.data.items
+	end
+ 
+  def init_calendar
+		@client = Google::APIClient.new(
+			:application_name => 'Scheudler',
+			:application_version => '1.0.0'
+		)
+    @client.authorization.access_token = session[:token]
+		@calendar = @client.discovered_api('calendar', API_VERSION)
+  end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,29 @@
 class Event < ActiveRecord::Base
+	require 'json'
+
 	belongs_to :group
 	has_many :participants
 	has_many :users, :through => :participants
+	attr_accessor :gcal_id
+  attr_accessor :title
+	attr_accessor :start
+	attr_accessor :end
+	attr_accessor 
+
+	def to_json
+		event = {
+      'id' => self.gcal_id,
+      'title' => self.name,
+      'start' => self.start.dateTime.strftime('%Y-%m-%d %H:%M:%S'),
+      'end' => self.end.dateTime.strftime('%Y-%m-%d %H:%M:%S')
+    }
+	end
+
+  def convert_to_gcal_event
+    event = {
+      'summary' => self.name,
+      'start' => self.start,
+      'end' =>  self.end
+    }
+  end
 end
