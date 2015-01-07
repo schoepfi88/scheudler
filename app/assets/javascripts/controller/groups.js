785a79c426e0c317e457d1b5b4dd9c898bb2993e
Alexander Jungmann <blingwing093@gmail.com>
2015-01-07 19:17:34 +0100

    Updated reloading of templates on change for groups

--- app/assets/javascripts/controller/groups.js
+++ app/assets/javascripts/controller/groups.js
@@ -1,18 +1,18 @@
-angular.module('scheudler').controller("groupsCtrl", function($scope,groupsService,Util){
+angular.module('scheudler').controller("groupsCtrl", function($scope,groupsService,Util,$templateCache){
 	$scope.isGoogleUser = false;
 	$scope.create_group = function(isValid){
 		if(isValid && $scope.isGoogleUser){
 			groupsService.group.create($scope.groupData, function(){
+				$templateCache.remove('/templates/groups');
 				location.href="/#/groups";
-				location.reload();
 			});
 		}
 	};
 
 	$scope.delete_group = function(id){
 		groupsService.group.destroy(id, function(){
+			$templateCache.remove('/templates/groups');
 			location.href="/#/groups";
-			location.reload();
 		});
 	};
 
@@ -20,8 +20,9 @@ angular.module('scheudler').controller("groupsCtrl", function($scope,groupsServi
 		$scope.removeData.group_id = group_id;
 		$scope.removeData.user_id = user_id;
 		groupsService.group.remove($scope.removeData, function(){
+			$templateCache.remove('/templates/groups_dashboard/' + group_id + '/members');
+			$templateCache.remove('/templates/groups_dashboard/' + group_id);
 			location.href="/#/groups_dashboard/" + group_id;
-			location.reload();
 		});
 	};
 
@@ -66,8 +67,8 @@ angular.module('scheudler').controller("groupsCtrl", function($scope,groupsServi
 		if(isValid){
 			$scope.inviteData.group_id = id;
 			groupsService.group.invite($scope.inviteData, function(){
+				$templateCache.remove('/templates/groups_dashboard/' + id);
 				location.href="/#/groups_dashboard/" + id;
-				location.reload();
 			});
 		}
 	};
