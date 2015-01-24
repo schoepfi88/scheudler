685a70ddc1bd7920cfe0e8771371e497d4561eae
raffi92 <raphael.gwiggner@gmail.com>
2015-01-24 12:28:03 +0100

    ll

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150116181151) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "admins", force: true do |t|
     t.integer  "group_id"
     t.integer  "user_id"
